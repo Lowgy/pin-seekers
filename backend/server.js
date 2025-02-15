a8ca78a6967a63c54130c61f19bce1ce6ec68d1d
Logan Olson <loganolsonn@gmail.com>
2025-02-14 20:40:00 -0600

    Simple Routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/courses', (req, res) => {});
+
+app.post('/register', (req, res) => {});
+
+app.post('/login', (req, res) => {});
+
+app.delete('/review/:id', (req, res) => {});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
